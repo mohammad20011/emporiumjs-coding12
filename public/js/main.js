5a89a2f5efe330ed1a4c60e056e4a24412f2bfd7
Mohammad <sahlimomo2001@gmail.com>
2020-02-14 14:46:53 +0100

    section5

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,18 @@
 let body = document.querySelector("body")
 let btnBlanc = document.getElementById("blanc")
 let btnNoir = document.getElementById("noir")
-
+let nav = document.querySelector("nav")
 btnBlanc.addEventListener("click", ()=>{
     body.style.backgroundColor = "white"
     body.style.color = "black"
+    nav.classList.remove('navbar-dark')
+    nav.classList.add('navbar-light')
 })
 btnNoir.addEventListener("click", ()=>{
     body.style.backgroundColor = "black"
     body.style.color = "white"
+    nav.classList.remove('navbar-light')
+    nav.classList.add('navbar-dark')
 })
+
+let h5 = document.getSelection
