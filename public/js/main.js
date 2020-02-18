6a57f298517f9ef560827fc386a68df1ef9fa37a
Mohammad <sahlimomo2001@gmail.com>
2020-02-18 09:15:49 +0100

    final

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ let body = document.querySelector("body")
 let btnBlanc = document.getElementById("blanc")
 let btnNoir = document.getElementById("noir")
 let nav = document.querySelector("nav")
+let trust1 = document.getElementById("trust1")
 btnBlanc.addEventListener("click", ()=>{
     body.style.backgroundColor = "white"
     body.style.color = "black"
@@ -13,6 +14,29 @@ btnNoir.addEventListener("click", ()=>{
     body.style.color = "white"
     nav.classList.remove('navbar-light')
     nav.classList.add('navbar-dark')
+    trust1.classList.remove('navbar-light')
 })
 
-let h5 = document.getSelection
+// Modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// Bouton connexion
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Bouton fermer le modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Fermer le modal en cliquant en dehors
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// } 
+
+
